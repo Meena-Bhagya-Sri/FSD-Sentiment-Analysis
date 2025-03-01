64daa7cc6149c3d5f9d4688f315b550471e1e94e
Meenu Bhagya Sri <meenabhagyasri668@gmail.com>
2025-03-01 14:55:27 +0530

    Initial commit

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ def dashboard():
     return render_template('dashboard.html', chart='static/chart.png', reviews=reviews)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
